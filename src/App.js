7917e6e7f3db1f67f41daefa0c21f3f6aedbf54c
Mikolaj Kluczniak <mikolajkluczniak@MacBook-Pro-Mikolaj.local>
2022-11-29 20:12:08 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react"
+import data from "./data"
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <h2>lorem ipsum project setup</h2>
+}
+
+export default App
